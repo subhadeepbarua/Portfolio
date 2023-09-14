b698f2a21269c650b24139b63262477e1e0b834e
Subhadeep Barua <83444796+subhadeepbarua@users.noreply.github.com>
2023-09-14 14:16:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+
+import React from 'react';
+import { Route, Router, Routes } from 'react-router-dom';
+import MainBody from './components/mainBody/MainBody';
+import Admin from "./components/admin/Admin";
+
+
+
+function App() {
+  return (
+    <>
+    <Routes>
+    {/* <Route path='/Navbar' element={<Navbar/>}/>
+    <Route path='/' element={<Intro/>}/>
+    <Route path='/Skill' element={<Skill/>}/>
+    <Route path='/Projects' element={<Projects/>}/> */}
+    <Route path='/' element={<MainBody/>}/>
+    <Route path='/Admin' element={<Admin/>}/>
+    {/* <Route path='/Footer' element={<Footer/>}/> */}
+    </Routes>
+      {/* <Navbar/>
+      <Intro/>
+      <Skill/>
+      <Routes>
+        <Route path="/" element={<Projects />} />
+        <Route exact path="/admin" Component={Admin}/>
+      </Routes>
+      <Footer/> */}
+    </>
+  );
+}
+
+export default App;
